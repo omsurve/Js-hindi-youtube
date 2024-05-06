77b99fa8931d4af5d74f560e7e76035421db61c0
omsurve <116489053+omsurve@users.noreply.github.com>
2024-05-06 13:06:40 +0000

    conversion operations

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = null
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueIntnumber = Number(score)
+console.log(typeof valueIntnumber)
+console.log(valueIntnumber)
+
+//If we convert type into number
+// "33" => 33
+// "123Om" => NaN
+// true => 1 / false => 0
+
+// Type of after conversion
+// null => object
+// undefined => Nan
+
+
+let isLloggedIn = "Om"
+let booleanIsLoggedIn = Boolean(isLloggedIn) 
+
+// True => 1/ False => 0
+// "om" => true / "" => false 
+
+
+let somenumber = 123
+let stringNumber = String(somenumber)
+console.log(typeof stringNumber) // => string
+console.log(stringNumber) // => 123
+
