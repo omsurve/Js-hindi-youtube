c5fc290d6838ba2ff8f285734104170cd8384706
omsurve <116489053+omsurve@users.noreply.github.com>
2024-05-08 18:38:48 +0530

    datatypes-summary

---
+++ Basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 Types: 
+        // String, Number, Boolean, Null, Undefined, Symbol, BigInt 
+
+const score =100
+const scorerValue = 100.30
+
+const isLoggedIn = true
+const temperature = null
+let userEmail;
+
+const symbole = Symbol("123")
+const symbole2 = Symbol("123")
+
+console.log(symbole===symbole2);
+
+const bigInt = 233355665566655555n
+
+
+// Referance Type:
+                // Array, Function, Object
+
+const names = ["Om","Raj","Shree","Bhavesh"]
+
+let myObj={
+        "Name":"Om",
+        "Age":21,
+        "Address":"nisarga colony jalgaon"
+}
+
+const myFun =function (){
+        console.log("Hello World");
+}
