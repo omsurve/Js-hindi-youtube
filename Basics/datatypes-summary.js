3c5998a9bad20d98ffa19fa75cf74a46cf06ff89
omsurve <116489053+omsurve@users.noreply.github.com>
2024-05-08 19:05:42 +0530

    stack and heap

--- Basics/datatypes-summary.js
+++ Basics/datatypes-summary.js
@@ -21,14 +21,50 @@ const bigInt = 233355665566655555n
 // Referance Type:
                 // Array, Function, Object
 
+
+// Array
 const names = ["Om","Raj","Shree","Bhavesh"]
 
+//object
 let myObj={
         "Name":"Om",
         "Age":21,
         "Address":"nisarga colony jalgaon"
 }
 
+//Function
 const myFun =function (){
         console.log("Hello World");
 }
+
+/*-------------------------------------------------------------------------------------------------------------------- */
+
+
+// Stack (Primitive), Heape (Non-Premitive)
+
+// Primitive
+let myname = "Om"
+let anotherName = myname
+
+anotherName = "Omdyaa"
+
+
+console.log(myname);
+console.log(anotherName);//This value will not change 
+
+
+// Non-premitive
+let user1 = {
+        "Name" : "Om",
+        "Age": "21",
+        "Email": "omsurve@gmail.com"
+}
+
+let user2 = user1
+console.log(user1.Email);
+console.log(user2.Email);
+
+user2.Email = "om@gmail.com"
+
+console.log(user1.Email);
+console.log(user2.Email);//This value will change in both user1 and user2
