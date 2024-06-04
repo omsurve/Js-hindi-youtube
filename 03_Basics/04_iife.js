8e65825931ba330aeacea04d029eb25e3a4c74a9
omsurve <116489053+omsurve@users.noreply.github.com>
2024-06-04 23:40:28 +0530

    one js

--- 03_Basics/04_iife.js
+++ 03_Basics/04_iife.js
@@ -3,7 +3,7 @@
 (function chai() {
     // name IIFE
     console.log(`DB CONNECTED`);
-})();
+})();                              // If you want to write two iife then we havve to add semicolon (;)
 
 ((name)=>{
     console.log(`DB CONNECTED, ${name}`);
